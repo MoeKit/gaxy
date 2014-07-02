03b4c4c81c047a4d372567ae8af13504aef9568f
airyland <airyland@qq.com>
2014-07-02 10:34:21 +0800

    remove unused code

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require('express'),
     app = express(),
     fs = require('fs'),
     http = require('http');
-var server = app.listen(8877);
+var server = app.listen(8877,function(){
+    console.log('server is running on 8877');
+});
 
 
 var data = require('./ga.js');
-
-app.use(express.methodOverride());
 
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -15,28 +15,6 @@ app.all('*', function (req, res, next) {
     next();
 });
 
-// home page
-app.get('/', function (req, res) {
-    res.send('Hello, I\'m seedit Data Visualization Center');
-});
-
-var httpGet = function (url, callback) {
-    var http = require('http');
-    http.get(url, function (rs) {
-        var data = '';
-        rs.on('data', function (chunk) {
-            data += chunk;
-        });
-        rs.on('end', function () {
-            callback(data);
-        });
-    })
-
-};
-
-
-// 获取 google 统计
-// 使用代理服务器
 app.get('/api/ga.json', function (req, res) {
     var search = req.originalUrl.replace('/api/ga.json?', '');
     data.get_raw_data(search, function (data) {
