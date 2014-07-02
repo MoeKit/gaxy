4acd905af48aed856a67edb706600331120ee5f5
airyland <airyland@qq.com>
2014-07-02 10:31:50 +0800

    add cache dir

--- app.js
+++ app.js
@@ -4,46 +4,10 @@ var express = require('express'),
     http = require('http');
 var server = app.listen(8877);
 
-/**
- * Removes a module from the cache
- */
-require.uncache = function (moduleName) {
-    // Run over the cache looking for the files
-    // loaded by the specified module name
-    require.searchCache(moduleName, function (mod) {
-        delete require.cache[mod.id];
-    });
-};
-
-/**
- * Runs over the cache to search for all the cached
- * files
- */
-require.searchCache = function (moduleName, callback) {
-    // Resolve the module identified by the specified name
-    var mod = require.resolve(moduleName);
-
-    // Check if the module has been resolved and found within
-    // the cache
-    if (mod && ((mod = require.cache[mod]) !== undefined)) {
-        // Recursively go over the results
-        (function run(mod) {
-            // Go over each of the module's children and
-            // run over it
-            mod.children.forEach(function (child) {
-                run(child);
-            });
-            // Call the specified callback providing the
-            // found module
-            callback(mod);
-        })(mod);
-    }
-};
 
 var data = require('./ga.js');
 
 app.use(express.methodOverride());
-app.use(express.static(__dirname + '/cache'));
 
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
